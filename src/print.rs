ca4e0f6ac69acb7e14968967ce817b7f2a0f4c8b
Michal Gašparík <michal@successful.click>
2021-02-13 14:24:45 +0100

    Print Functions - Named Arguments

--- src/print.rs
+++ src/print.rs
@@ -8,4 +8,8 @@ pub fn run() {
     // Positional Arguments
     println!("{0} is from {1} and {0} likes to {2}",
              "Michal", "Bratislava", "code");
+
+    // Named Arguments
+    println!("{name} likes to play {activity}",
+             name = "Michal", activity = "AirSoft");
 }
