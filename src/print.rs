b57b172431d1e1eefd1155926c44c52a053be63d
Michal Gašparík <michal@successful.click>
2021-02-13 14:11:39 +0100

    Print Functions - Positional Argiments

--- src/print.rs
+++ src/print.rs
@@ -1,4 +1,11 @@
 pub fn run() {
     // Print to console
     println!("Hello from the print.rs");
+
+    // Basic Formatting
+    println!("{} is from {}", "Michal", "Bratislava");
+
+    // Positional Arguments
+    println!("{0} is from {1} and {0} likes to {2}",
+             "Michal", "Bratislava", "code");
 }
